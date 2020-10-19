0f000dfbdf93cacb7442d5845d3ce90ce9b9ba92
yesminee <43929137+yesminee@users.noreply.github.com>
2020-10-19 07:50:23 +0100

    Add files via upload

---
+++ src/ressources.js
@@ -0,0 +1,109 @@
+const ressources=[{
+    title: "Why so serious",
+        rate: 1,
+            imgSrc: "assets/whysoserious.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Avatar",
+        rate: 2,
+            imgSrc: "assets/avatar.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Inception",
+        rate: 5,
+            imgSrc: "assets/inception.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Twilight",
+        rate: 3,
+            imgSrc: "assets/twilight.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "The karate kid",
+        rate: 4,
+            imgSrc: "assets/karatekid.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Venom",
+        rate: 5,
+            imgSrc: "assets/venom.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Interstellar",
+        rate: 4,
+            imgSrc: "assets/interstellar.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Next",
+        rate: 1,
+            imgSrc: "assets/next.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Iron Man",
+        rate: 4,
+            imgSrc: "assets/ironman.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Jocker",
+        rate: 5,
+            imgSrc: "assets/jocker.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Pokemon",
+        rate: 3,
+            imgSrc: "assets/pokemon.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },
+{
+    title: "Aladin",
+        rate: 2,
+            imgSrc: "assets/aladin.jpg",
+                description:
+    "Enjoy the best streaming performance and full HD image quality",
+        posterUrl: "https://www.netflix.com/tn-fr/",
+            details: "See details..",
+    },];
+export default ressources;
